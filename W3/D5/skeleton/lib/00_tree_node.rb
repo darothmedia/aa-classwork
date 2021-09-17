e7b78b8f24f1a0f70db42e427fb15a6b7342c9d9
shuang zheng <shuangzheng16@gmail.com>
2021-09-17 15:25:23 -0700

    Initialization and define Parent= (sz)

--- W3/D5/skeleton/lib/00_tree_node.rb
+++ W3/D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    unless node == nil
+      @parent = node 
+      node.children << self unless node.children.include?(self)
+    end 
+  end
+
 
 end
