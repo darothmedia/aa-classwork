4f475215cace353bea8db9281741ca77cb79bf95
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-09-20 15:09:21 -0700

    Progress on knights travails [ar]

--- W4/D1/trees/lib/knights.rb
+++ W4/D1/trees/lib/knights.rb
@@ -5,18 +5,46 @@ class KnightPathFinder
 
   def initialize(pos)
     @grid = Array.new(8){Array.new(8)}
-    @root_node = pos
+    @root_node = pos #[0,1]
     @move_tree = PolyTreeNode.new(@root_node)
     @considered_positions = [pos]
   end
 
   def self.valid_moves(pos)
-    
-    pos.each do |value|
-      @considered_positions << 
+    # return false if pos[0] > @grid.length || pos[0] < 0
+    # return false if pos[1] > @grid.length || pos[1] < 0
 
-    if pos[0] + 2 > @grid.length
-      ## [-1]
+    last_pos = @considered_positions[-1]
+
+    possible_pos = [
+      [(last_pos[0] - 2), (last_pos[1] - 1)], 
+      [(last_pos[0] - 1), (last_pos[1] - 2)], 
+      [(last_pos[0] + 2), (last_pos[1] - 1)], 
+      [(last_pos[0] + 1), (last_pos[1] - 2)], 
+      [(last_pos[0] + 2), (last_pos[1] + 1)],
+      [(last_pos[0] - 2), (last_pos[1] + 1)],
+      [(last_pos[0] - 1), (last_pos[1] + 2)],
+      [(last_pos[0] + 1), (last_pos[1] + 2)]
+    ]
+    valid_pos = []
+    possible_pos.each do |pospos|
+      next if pospos[0] > @grid.length || pospos[0] < 0
+      next if pospos[1] > @grid.length || pospos[1] < 0
+      valid_pos << pospos
+    end
+    valid_pos.include?(pos)
+    # [-1][-2], [-2][-1], [1][-2], [2][-1], [-1][2], [-2][1]
+    # [1][2]
+    # [2][1]]
+  end
+
+  KnightPathFinder.valid_moves([2,3])
+
+  class Array
+    def self.select
+  end
+
+  [1,2,3].select
 
 
 
