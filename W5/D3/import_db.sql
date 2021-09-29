969eb74ff93b1269f977ceb0e8fa8163b5fd8872
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-09-29 10:56:57 -0700

    Progress on DB [ar]

--- W5/D3/import_db.sql
+++ W5/D3/import_db.sql
@@ -1,3 +1,4 @@
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
 
@@ -11,20 +12,20 @@ CREATE TABLE questions (
 
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
-    user_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    questioner INTEGER NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (questioner) REFERENCES users(id)
 );
 
 
 CREATE TABLE question_follows (
 
     id INTEGER PRIMARY KEY, 
-    user_id INTEGER NOT NULL,
+    u_id INTEGER NOT NULL,
     questions_id INTEGER NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (u_id) REFERENCES users(id),
     FOREIGN KEY (questions_id) REFERENCES questions(id)
 
 
@@ -54,3 +55,29 @@ CREATE TABLE question_likes (
     FOREIGN KEY (question) REFERENCES questions(id)
 
 );
+
+INSERT INTO
+    users (f_name, l_name)
+VALUES
+    ('Tyler', 'Harper'),
+    ('Adrian', 'Rothschild');
+
+INSERT INTO
+    questions (title, body, questioner)
+VALUES
+    ('byebug', 'How do you install the gem byebug?', 1);
+
+INSERT INTO
+    question_follows (u_id, questions_id)
+VALUES
+    (2, 1);
+
+INSERT INTO
+    replies (subject_question, parent_reply, user, body)
+VALUES
+    (1, NULL, 2, 'Type bundle install!');
+
+INSERT INTO
+    question_likes (liker, question)
+VALUES
+    (2, 1);
