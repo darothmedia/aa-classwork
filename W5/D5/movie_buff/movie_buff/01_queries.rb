f846479e2a7d83bf2ff8336faf39a09ba14831b8
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-01 12:03:47 -0700

    Progress on Movie Buff [ar]

--- W5/D5/movie_buff/movie_buff/01_queries.rb
+++ W5/D5/movie_buff/movie_buff/01_queries.rb
@@ -7,6 +7,8 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.where(score: 2.0..3.0)
+  .select(:id, :title, :score)
 end
 
 def harrison_ford
@@ -20,6 +22,8 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Movie.joins(:actors).select(:id, :title).where("actors.name = 'Harrison Ford' AND castings.ord != 1")
 
 end
 
