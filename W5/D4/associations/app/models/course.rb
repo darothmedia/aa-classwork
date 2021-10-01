c241bba70c3718fab02368e06915c08cb883ae51
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-09-30 23:57:44 -0700

    W5D4 add

---
+++ W5/D4/associations/app/models/course.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+class Course < ApplicationRecord
+
+  belongs_to :prereqs,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+  
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :students
+
+end
---
+++ W5/D4/associations/app/models/enrollment.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Enrollment < ApplicationRecord
+
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+
+  belongs_to :students,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+
+end
