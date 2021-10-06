3e15b9d601ceac4c0a7730f45b9e3b2a97f787b6
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-05 17:20:33 -0700

    Progress on W6D2

--- W6/D2/active-record/lib/01_sql_object.rb
+++ W6/D2/active-record/lib/01_sql_object.rb
@@ -5,21 +5,46 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    res = DBConnection.execute2(<<-SQL)
+    SELECT
+    *
+    FROM
+    '#{self.table_name}'
+    SQL
+    arr = []
+    res[0].each {|el| arr << el.to_sym}
+    arr
   end
 
   def self.finalize!
+    self.columns.each do |el|
+      define_method(el) do
+        instance_variable_get("@#{el}")
+      end
+      define_method("#{el}=") do |value|
+        instance_variable_set("@#{el}", value)
+      end
+    end
+    
   end
 
   def self.table_name=(table_name)
-    table_name = self.to_s
-    # string = self.to_s[0].downcase + self.to_s[1..-1] + 's'
-    # table_name ||= string
+    table_name ||= self.table_name
     table_name
   end
 
   def self.table_name
-    
+    str = ""
+    self.to_s.each_char.with_index do |char, i|
+      if i == 0
+        str += char.downcase 
+      elsif char == char.upcase && i > 0
+        str += ("_" + char.downcase)
+      else
+        str += char
+      end
+    end
+    str + "s"
   end
 
   def self.all
