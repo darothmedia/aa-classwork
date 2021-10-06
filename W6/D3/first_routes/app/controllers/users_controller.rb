862eb93e764f03a3413bcc08a698936a0c4a4c16
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-10-06 15:21:36 -0700

    Starts Art Share API

--- W6/D3/first_routes/app/controllers/users_controller.rb
+++ W6/D3/first_routes/app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    render plain: "Hello World!"
+  end
+
+  def create
+    # user = User.new(
+    #   params.require(:user).permit(:username, :email, :age)
+    # )
+    # user.save
+    render json: user
+  end
+
+  def show
+    render json: params
   end
 end
