cd9e55f37e19e5dc123f401a241e81a46f7b15a0
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-13 14:31:37 -0700

    Progress on goals_app

---
+++ W7/D3/goal_app/spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:username)}
+  it { should validate_presence_of(:password_digest)}
+  it { should validate_presence_of(:session_token)}
+  it { should validate_length_of(:password).is_at_least(6)}
+  it { should have_many(:goals)}
+
+  describe "Uniqueness" do
+    before :each do
+      create(:user)
+    end
+    it { should validate_uniqueness_of(:username)}
+    it { should validate_uniqueness_of(:session_token)}
+  end
+end
