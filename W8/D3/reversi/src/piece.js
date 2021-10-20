aa61f04acc4e008b0650b594686f47d34e34bb65
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-20 11:49:37 -0700

    finish Piece [ar]

--- W8/D3/reversi/src/piece.js
+++ W8/D3/reversi/src/piece.js
@@ -2,18 +2,25 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === "black") {
+        return "white";
+    } else {
+        return "black";
+    }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    this.color = this.oppColor()
 };
 
 /**
@@ -21,6 +28,11 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    if (this.color === "black") {
+        return "B";
+    } else {
+        return "W";
+    }
 };
 
 // DON'T TOUCH THIS CODE
