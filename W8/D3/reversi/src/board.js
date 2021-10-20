0fc02e84c64280952d0c7fdfd526bca09739b220
wesley <cheung.w.2016@gmail.com>
2021-10-20 16:12:11 -0700

    finished placePiece

--- W8/D3/reversi/src/board.js
+++ W8/D3/reversi/src/board.js
@@ -148,6 +148,23 @@ Board.prototype.validMove = function (pos, color) {
  * Throws an error if the position represents an invalid move.
  */
 Board.prototype.placePiece = function (pos, color) {
+  if (!this.validMove(pos, color)) {
+    throw new Error("Invalid move!");
+  } else {
+    this.grid[pos[0]][pos[1]] = new Piece(color);
+    for (let i = 0; i < Board.DIRS.length; i++) {
+      let flip = this._positionsToFlip(pos, color, Board.DIRS[i]);
+      if (flip.length > 0) {
+        flip.forEach(pos => this.getPiece(pos).flip());
+      };
+    }
+  }
+
+
+
+  
+
+
 };
 
 /**
